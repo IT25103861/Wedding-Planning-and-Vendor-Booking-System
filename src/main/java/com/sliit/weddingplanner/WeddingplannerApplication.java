a2e937905de1a73181183551e191ac50344e5639
IT25103861 <it25103861@my.sliit.lk>
2026-04-18 08:42:30 +0530

    initial commit

---
+++ src/main/java/com/sliit/weddingplanner/WeddingplannerApplication.java
@@ -0,0 +1,14 @@
+package com.sliit.weddingplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingplannerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("test3");
+		SpringApplication.run(WeddingplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sliit/weddingplanner/WeddingplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.weddingplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
