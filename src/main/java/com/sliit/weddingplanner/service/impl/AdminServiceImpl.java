0d1032ba483c7905d7eb7f4664735da182a5dbad
IT25103861 <it25103861@my.sliit.lk>
2026-05-11 20:34:51 +0530

    feat: update AdminServiceImpl with proper exception handling

--- src/main/java/com/sliit/weddingplanner/service/impl/AdminServiceImpl.java
+++ src/main/java/com/sliit/weddingplanner/service/impl/AdminServiceImpl.java
@@ -1,6 +1,8 @@
 package com.sliit.weddingplanner.service.impl;
 
 import com.sliit.weddingplanner.dto.admin.AdminDTO;
+import com.sliit.weddingplanner.exeption.DuplicateRecordException;
+import com.sliit.weddingplanner.exeption.ResourceNotFoundException;
 import com.sliit.weddingplanner.repository.AdminRepository;
 import com.sliit.weddingplanner.service.AdminService;
 import org.springframework.stereotype.Service;
@@ -16,16 +18,11 @@ public class AdminServiceImpl implements AdminService {
         this.adminRepository = adminRepository;
     }
 
+
     @Override
     public AdminDTO createUser(AdminDTO adminDTO) {
-
-        boolean exists = adminRepository.existsByUsernameOrEmail(
-                adminDTO.getUsername(),
-                adminDTO.getEmail()
-        );
-
-        if (exists) {
-            throw new RuntimeException("Username or email already exists");
+        if (adminRepository.existsByUsernameOrEmail(adminDTO.getUsername(), adminDTO.getEmail())) {
+            throw new DuplicateRecordException("Username or email already exists");
         }
 
         return adminRepository.save(adminDTO);
@@ -34,7 +31,7 @@ public class AdminServiceImpl implements AdminService {
     @Override
     public AdminDTO getUserById(int id) {
         return adminRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Admin not found"));
+                .orElseThrow(() -> new ResourceNotFoundException("Admin not found with id " + id));
     }
 
     @Override
@@ -43,13 +40,24 @@ public class AdminServiceImpl implements AdminService {
     }
 
     @Override
-    public AdminDTO updateUser(int id, AdminDTO dto) {
-        dto.setId(id);
-        return adminRepository.update(dto);
+    public AdminDTO updateUser(int id, AdminDTO adminDTO) {
+        AdminDTO admin = getUserById(id);
+
+        admin.setName(adminDTO.getName());
+        admin.setUsername(adminDTO.getUsername());
+        admin.setEmail(adminDTO.getEmail());
+
+        return adminRepository.update(admin);
     }
 
     @Override
     public void deleteUser(int id) {
-        adminRepository.delete(id);
+        AdminDTO admin = getUserById(id);
+        if(admin == null){
+            System.out.println("No Admin Found under "+id+" number");
+        }else{
+            adminRepository.delete(admin.getId());
+        }
+
     }
 }
